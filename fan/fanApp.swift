1959e22d6c1b48d294822c0bcf8ab2fc452d6327
mohamadlounnas <mohamadlounnas@gmail.com>
2026-01-11 20:40:54 +0100

    Initial Commit

---
+++ fan/fanApp.swift
@@ -0,0 +1,17 @@
+//
+//  fanApp.swift
+//  fan
+//
+//  Created by mohamad on 11/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct fanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
